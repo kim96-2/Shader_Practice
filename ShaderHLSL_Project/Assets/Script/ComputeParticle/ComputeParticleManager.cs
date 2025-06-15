60da4eba494089eb892010ade4c7bd7420ff1bac
Lee <totolikid@naver.com>
2025-06-15 18:00:51 +0900

    particle compute shader에서 충돌체 기능 + flocking 기능 + 색깔 변경 기능 추가
    그 외로 다양한 flocking 관련 기능 추가

--- ShaderHLSL_Project/Assets/Script/ComputeParticle/ComputeParticleManager.cs
+++ ShaderHLSL_Project/Assets/Script/ComputeParticle/ComputeParticleManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ComputeParticleManager : MonoBehaviour
@@ -8,23 +9,47 @@ public class ComputeParticleManager : MonoBehaviour
     {
         public Vector3 position;
         public Vector3 velocity;
-        public float life;
+        public float headAmount;
+        public float colorAmount;
     }
 
-    const int SIZE_PARTICLE = 7 * sizeof(float);
+    struct Obstacle
+    {
+        public Vector3 position;
+        public float size;
+    }
+
+    const int SIZE_PARTICLE = 8 * sizeof(float);
+    const int SIZE_OBSTACLE = 4 * sizeof(float);
 
     [SerializeField] Mesh particleMesh;
     [SerializeField] Material particleMaterial;
     [SerializeField] int count;
     [SerializeField] ComputeShader computeShader;
 
-    [Space(15f)]
-    [SerializeField] Transform target;
-    
+    [Space(10f)]
+    [SerializeField] AnimationCurve headAmountCurve;
+
+    [Header("Update Setting")]
+    [SerializeField] float nearRadius = 10f;
+    [SerializeField, Range(0f, 10f)] float separationAmount = 1f;
+    [SerializeField, Range(0f, 10f)] float alignmentAmount = 1f;
+    [SerializeField, Range(0f, 10f)] float cohesionAmount = 1f;
+
+    [Space(10f)]
+    [SerializeField, Range(0f, 100f)] float randomPowerAmount = 10f;
+
+    [Space(10f)]
+    [SerializeField] Transform mainBoundaryTransform;
+
+    [Space(10f)]
+    [SerializeField] Transform obstaclesTransform;
 
     ComputeBuffer particleBuffer;
-    int kernelID;
 
+    ComputeBuffer obstacleBuffer;
+
+    int kernelID;
     int groupSize;
 
     RenderParams rp;
@@ -33,17 +58,22 @@ public class ComputeParticleManager : MonoBehaviour
     void Start()
     {
         InitParticle();
+        InitObstacle();
     }
 
     void Update()
     {
-        UpdateParticle();
+        UpdateObstacle();
+        UpdateParticle();      
     }
 
     void OnDisable()
     {
         if (particleBuffer != null)
             particleBuffer.Release();
+
+        if (obstacleBuffer != null)
+            obstacleBuffer.Release();
     }
 
     void InitParticle()
@@ -56,14 +86,12 @@ public class ComputeParticleManager : MonoBehaviour
             float y = Random.Range(-1f, 1f);
             float z = Random.Range(-1f, 1f);
             Vector3 pos = new Vector3(x, y, z);
-            pos.Normalize();
 
-            pos *= Random.value * 5f;
+            particles[i].position = pos.normalized * Random.value * mainBoundaryTransform.localScale.x / 4f;
+            particles[i].velocity = Random.rotation * Vector3.forward * Random.Range(5f, 10f);
+            particles[i].headAmount = headAmountCurve.Evaluate(Random.value);
 
-            particles[i].position = pos;
-            particles[i].velocity = Vector3.zero;
-
-            particles[i].life = Random.value * 5.0f + 1.0f;
+            particles[i].colorAmount = Random.value;
         }
 
         particleBuffer = new ComputeBuffer(count, SIZE_PARTICLE);
@@ -78,21 +106,66 @@ public class ComputeParticleManager : MonoBehaviour
         computeShader.SetBuffer(kernelID, "_ParticleBuffer", particleBuffer);
         particleMaterial.SetBuffer("_ParticleBuffer", particleBuffer);
 
+        computeShader.SetInt("_ParticleCount", count);
+
         rp = new RenderParams(particleMaterial);
-        rp.worldBounds = new Bounds(Vector3.zero, 1000f * Vector3.one);
+        rp.worldBounds = new Bounds(Vector3.zero, 100f * Vector3.one);
         bounds = rp.worldBounds;
     }
 
     void UpdateParticle()
     {
-        Vector3 pos = target.position;
 
-        computeShader.SetFloat("_Time", Time.deltaTime);
-        computeShader.SetVector("_TargetPosition", new Vector4(pos.x, pos.y, pos.z, 1));
+        computeShader.SetVector("_Time", new Vector4(Time.time, Time.deltaTime, 0, 0));
+        computeShader.SetFloat("_NearRadius", nearRadius);
+
+        computeShader.SetVector("_SACParams", new Vector4(separationAmount, alignmentAmount, cohesionAmount, 0));
+
+        computeShader.SetFloat("_RandomPowerAmount", randomPowerAmount);
+
+        computeShader.SetVector("_MainBoundary", new Vector4(mainBoundaryTransform.position.x, mainBoundaryTransform.position.y, mainBoundaryTransform.position.z, mainBoundaryTransform.localScale.x / 2f));
 
         computeShader.Dispatch(kernelID, groupSize, 1, 1);
 
         //Graphics.RenderPrimitives(rp, MeshTopology.Points, 1, count);
         Graphics.DrawMeshInstancedProcedural(particleMesh, 0, particleMaterial, bounds, count);
+    }
+
+    void InitObstacle()
+    {
+        List<Obstacle> obstacles = new List<Obstacle>();
+        foreach (Transform tr in obstaclesTransform)
+        {
+            Obstacle obs = new Obstacle();
+            obs.position = tr.position;
+            obs.size = tr.localScale.x / 2f;
+            obstacles.Add(obs);
+
+            //Debug.Log(obs.position + " " + obs.size);
+        }
+
+        Obstacle[] obstaclesArray = obstacles.ToArray();
+
+        obstacleBuffer = new ComputeBuffer(obstaclesArray.Length, SIZE_OBSTACLE);
+        obstacleBuffer.SetData(obstaclesArray);
+
+        computeShader.SetBuffer(kernelID, "_ObsticleBuffer", obstacleBuffer);
+        computeShader.SetInt("_ObsticleCount", obstaclesArray.Length);
+    }
+
+    void UpdateObstacle()
+    {
+        List<Obstacle> obstacles = new List<Obstacle>();
+        foreach (Transform tr in obstaclesTransform)
+        {
+            Obstacle obs = new Obstacle();
+            obs.position = tr.position;
+            obs.size = tr.localScale.x / 2f;
+            obstacles.Add(obs);
+        }
+
+        Obstacle[] obstaclesArray = obstacles.ToArray();
+
+        obstacleBuffer.SetData(obstaclesArray);
     }
 }
--- ShaderHLSL_Project/Assets/ShaderCode/ComputeParticleShader/ComputeParticleShader.shader
+++ ShaderHLSL_Project/Assets/ShaderCode/ComputeParticleShader/ComputeParticleShader.shader
@@ -24,12 +24,14 @@
             struct Particle{
                 float3 position;
                 float3 velocity;
-                float life;
+                float headAmount;
+                float colorAmount;
             };
 
             StructuredBuffer<Particle> _ParticleBuffer;
 
             #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+             #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
 
             CBUFFER_START(UnityPerMaterial)
 
@@ -37,10 +39,36 @@
 
             CBUFFER_END
 
+            float4x4 create_matrix(float3 pos, float3 dir, float3 up) {
+                float3 zaxis = normalize(dir);
+                float3 xaxis = normalize(cross(up, zaxis));
+                float3 yaxis = cross(zaxis, xaxis);
+                float4x4 TR_Mat = float4x4(
+                        xaxis.x, yaxis.x, zaxis.x, pos.x,
+                        xaxis.y, yaxis.y, zaxis.y, pos.y,
+                        xaxis.z, yaxis.z, zaxis.z, pos.z,
+                        0, 0, 0, 1
+                    );
+
+                return mul(TR_Mat, float4x4(
+                        _PointSize, 0, 0, 0,
+                        0, _PointSize, 0, 0,
+                        0, 0, _PointSize, 0,
+                        0, 0, 0, 1
+                    ));
+            }
+
+            float3 Unity_ColorspaceConversion_RGB_RGB_float(float3 In)
+            {
+                float4 K = float4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+                float3 P = abs(frac(In.xxx + K.xyz) * 6.0 - K.www);
+                return In.z * lerp(K.xxx, saturate(P - K.xxx), In.y);
+            }
 
             struct Attributes
             {
                 float4 positionOS   : POSITION;
+                float3 normalOS     : NORMAL;
                 uint instanceID : SV_InstanceID;
                 UNITY_VERTEX_INPUT_INSTANCE_ID
             };
@@ -48,7 +76,9 @@
             struct Varyings
             {
                 float4 positionHCS  : SV_POSITION;
-                float4 color : COLOR;
+                float3 normalWS     : TEXCOORD0;
+                float color         : COLOR;
+                float4 uv            : TEXCOORD1;
             };
 
             Varyings vert(Attributes IN)
@@ -61,23 +91,35 @@
                     //Particle particle = (Particle)0;
                 //#endif
 
-                // Color
-			    float lerpVal = particle.life * 0.25f;
-			    OUT.color = half4(1.0f - lerpVal+0.1, lerpVal+0.1, 1.0f, lerpVal);
-
 			    // Position
-                //OUT.positionHCS = TransformObjectToHClip(particle.position);
-                OUT.positionHCS = TransformObjectToHClip(IN.positionOS * _PointSize + particle.position);
+                float4x4 modelMat = create_matrix(particle.position, normalize(particle.velocity), float3(0.0, 1.0, 0.0));
 
-                //OUT.size = _PointSize;
+                OUT.positionHCS = TransformObjectToHClip(mul(modelMat,IN.positionOS));
+
+                OUT.normalWS = TransformObjectToWorldNormal(IN.normalOS);
+
+                OUT.color = particle.colorAmount;//particle.headAmount;
+
+                OUT.uv = ComputeScreenPos(OUT.positionHCS);
 
                 return OUT;
             }
 
             half4 frag(Varyings IN) : SV_Target
             {
-                return IN.color;
-                //return 1;
+                half3 color = 0;
+
+                half3 diffuse = Unity_ColorspaceConversion_RGB_RGB_float(float3(IN.color, 0.9, 1.0));
+                
+                Light light= GetMainLight();
+
+                float ndotl = dot(normalize(IN.normalWS), light.direction);
+
+                half3 ambient = SampleSH(IN.normalWS) * diffuse;
+
+                color = saturate(ndotl) * diffuse + ambient;
+
+                return half4(color, 1);
             }
             ENDHLSL
         }
